2299cce54704447a9afe54978e998e7df16e0375
AndreyTorkhov <torkhovasic@yandex.ru>
2025-07-16 01:48:23 +0300

    Fix defaultTaskValues

--- src/shared/lib/getDefaultTaskValues.ts
+++ src/shared/lib/getDefaultTaskValues.ts
@@ -4,11 +4,9 @@ import type { TaskFormValues } from "@/features/TaskForm/model/schema";
  * Возвращает значения задачи по умолчанию
  */
 export const getDefaultTaskValues = (): TaskFormValues => ({
-  id: "",
   title: "",
   description: "",
   category: "Test",
   status: "To Do",
   priority: "Low",
-  createdAt: new Date().toISOString(),
 });
